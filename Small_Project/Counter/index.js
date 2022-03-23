c57b88bd02a3affe45e940d97ff5ccbd9c79331d
Prabh <prabhijeetsidhu@gmail.com>
2022-03-23 22:40:46 +0530

    Some update in counter app

--- Small_Project/Counter/index.js
+++ Small_Project/Counter/index.js
@@ -1,11 +1,43 @@
-const counting=document.querySelector(".number")
 let num=0;
+const counting=document.querySelector(".number")
 const incre=document.getElementById("increase")
 const decre=document.getElementById("decrease")
 const reset=document.getElementById("reset")
-const save=document.getElementById("save")
+const save=document.getElementById("save");
+const saveInput=document.getElementById("save-item");
+
 incre.addEventListener("click",function(){
   num+=1;
   counting.textContent=num;
-  counting.sytle.color="green";
+  color(num);
 })
+
+decre.addEventListener("click",function(){
+  num-=1;
+  counting.textContent=num;
+  color(num);
+})
+
+reset.addEventListener("dblclick",function(){
+  num=0;
+  counting.textContent=num;
+  saveInput.textContent="";
+  color(num);
+})
+
+save.addEventListener("click",function(){
+  saveInput.textContent+=num+" ";
+})
+
+function color(num){
+if(num>0){
+  counting.style.color="green";
+}
+else if(num<0){
+    counting.style.color="red";
+
+}
+else{
+    counting.style.color="black";
+
+}}
